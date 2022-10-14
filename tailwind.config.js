cf44321628b6f8900b5c3e59a0c16dccf0f43033
BowenYoung <yys958257943@gmail.com>
2022-10-14 20:31:27 +0800

    tailwind workplace upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+  },
+  variants: {
+
+    extend: {
+      backgroundColor: ['active'],
+    },
+  },
+  plugins: []
+  }
+}
